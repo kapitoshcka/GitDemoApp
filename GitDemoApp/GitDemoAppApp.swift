c73d551dc5a2a92f9692a5652b3dfe0cab909178
Philip Karnatsevich <kapitoshcka@gmail.com>
2024-10-24 14:14:17 -0700

    Initial Commit

---
+++ GitDemoApp/GitDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoAppApp.swift
+//  GitDemoApp
+//
+//  Created by Philip Karnatsevich on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
