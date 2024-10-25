1cc62d089328956e5227e75804237c173fc4f0d3
Philip Karnatsevich <kapitoshcka@gmail.com>
2024-10-25 15:40:28 -0700

    Update demo app with card view exercise(face up and face down)

--- GitDemoApp/ContentView.swift
+++ GitDemoApp/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Philip")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView(isFaceUp: true)
+            CardView()
         }
+        .foregroundColor(.purple)
         .padding()
+    }
+}
+
+struct CardView: View{
+    var isFaceUp: Bool = false
+    
+    var body: some View{
+        ZStack(content: {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("🎃").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
     }
 }
 
